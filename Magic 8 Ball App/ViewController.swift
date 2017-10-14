9356793af276b7381c36d7440a6fedd6db958a3c
Darragh Blake <darragh.blake48@gmail.com>
2017-10-14 22:52:13 +0100

    Changes to color

--- Magic 8 Ball App/ViewController.swift
+++ Magic 8 Ball App/ViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
+@IBDesignable
+
 class ViewController: UIViewController {
+    
+    var color1 = UIColor(red:0.16, green:0.67, blue:0.75, alpha:1.0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor(red:0.16, green:0.67, blue:0.75, alpha:1.0)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
+
+
+        
+
+    
+
 
