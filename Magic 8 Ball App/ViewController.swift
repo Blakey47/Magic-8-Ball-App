709eb7e31d0e3c74475be9752142d5f9bcfbcfc0
Darragh Blake <darragh.blake48@gmail.com>
2017-10-14 23:05:19 +0100

    Added function to the buttons and images

--- Magic 8 Ball App/ViewController.swift
+++ Magic 8 Ball App/ViewController.swift
@@ -12,13 +12,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var color1 = UIColor(red:0.16, green:0.67, blue:0.75, alpha:1.0)
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var randomBallIndex: Int = 0
+    
+    var ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5" ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(red:0.16, green:0.67, blue:0.75, alpha:1.0)
+        updateBallImage()
+    }
+    
+    func updateBallImage() {
+        randomBallIndex = Int(arc4random_uniform(5))
         
+        imageView.image = UIImage(named: ballArray[randomBallIndex])
+    }
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        updateBallImage()
     }
     
 }
